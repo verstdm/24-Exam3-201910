5ec811328aabf6e27e1f052a6df0df33a5addca1
verstdm <verstdm@rose-hulman.edu>
2018-10-25 21:44:33 -0400

    DOEN

--- src/problem5.py
+++ src/problem5.py
@@ -2,9 +2,9 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Dylan Verst.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -213,10 +213,15 @@ def problem5(x):
       :type x:    int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    c = x
+    while True:
+        s = sum_of_digits(c)
+        if is_prime(c) and is_prime(s):
+            return c
+        c = c + 1
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
