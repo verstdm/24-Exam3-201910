1d2640b0ec7a70bad6a65cb5355d51adfa1e1eac
verstdm <verstdm@rose-hulman.edu>
2018-10-25 21:35:14 -0400

    DOEN

--- src/problem4.py
+++ src/problem4.py
@@ -2,8 +2,8 @@
 Exam 3, problem 4.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October, 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Dylan Verst.  October, 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -77,13 +77,25 @@ And this one for n=14:
     :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     ####################################################################
     # IMPORTANT: In your final solution for this problem,
     #   you must NOT use string multiplication.
     ####################################################################
-
+    for k in range(n, 0, -1):
+        s = ''
+        for j in range(k - 1):
+            s = s + ' '
+        for j in range(1, n - k + 2):
+            s = s + str(j % 10)
+        s = s + ' '
+        for j in range(0, n - k + 2):
+            s = s + '*'
+        s = s + ' '
+        for j in range(k, 0, -1):
+            s = s + str(j % 10)
+        print(s)
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
